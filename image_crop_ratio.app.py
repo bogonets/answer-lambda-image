2fe1afff263a1976d2da63d584b886727979b75e
zer0 <osom8979@gmail.com>
2020-08-20 16:07:46 +0900

    Enable verbose message of the image_crop_ratio lambda.

--- image_crop_ratio.app.py
+++ image_crop_ratio.app.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import sys
+
+
+LOGGING_PREFIX = '[image.crop_ratio] '
+LOGGING_SUFFIX = '\n'
+
+verbose = False
+
+
+def print_out(message):
+    sys.stdout.write(LOGGING_PREFIX + message + LOGGING_SUFFIX)
+    sys.stdout.flush()
 
 
 def on_run(image: np.ndarray, roi: np.ndarray):
@@ -10,6 +22,11 @@ def on_run(image: np.ndarray, roi: np.ndarray):
     crop_y2 = int(h*y2)
     crop_x1 = int(w*x1)
     crop_x2 = int(w*x2)
+
+    if verbose:
+        print_out(f'image shape: {image.shape}')
+        print_out(f'roi: [{x1}, {y1}, {x2}, {y2}]')
+
     return {'output_image': image[crop_y1:crop_y2, crop_x1:crop_x2, :]}
 
 
