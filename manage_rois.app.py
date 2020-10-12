de1b65e82c79c9b62d81fcec043815756de75f65
sunhongyi <sunhongyi@gmail.com>
2020-10-12 18:00:41 +0900

    signal test ok

--- manage_rois.app.py
+++ manage_rois.app.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
 # @see <https://docs.scipy.org/doc/numpy-1.13.0/user/basics.types.html>
 
-import numpy as np
-
-
-def on_init():
-    return True
-
-
-def on_valid():
-    return True
-
-
 def on_run(preview):
-
-
-def on_destroy():
-    return True
+    pass
